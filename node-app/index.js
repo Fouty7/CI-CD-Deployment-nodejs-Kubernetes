8358943706c3c219337b58aa5780f0e1fca04b3d
Aminu <Amin4712@gmail.com>
2022-11-12 21:30:22 +0100

    workflow

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM CIRCLECI THIS WORK NOW!!! new code 001')
+    res.send('Hello World! FROM CIRCLECI THIS WORK NOW!!! new code 0066')
 })
 
 app.listen(port, () => {

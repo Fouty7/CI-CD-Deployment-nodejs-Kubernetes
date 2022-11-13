4cf167a696527afa8d11b979448e8838c80953aa
Aminu <Amin4712@gmail.com>
2022-11-13 16:35:26 +0100

    slack integration

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM CIRCLECI THIS WORK NOW!!! new code 0000000')
+    res.send('Hello World! FROM CIRCLECI THIS WORK NOW!!! testing slack')
 })
 
 app.listen(port, () => {

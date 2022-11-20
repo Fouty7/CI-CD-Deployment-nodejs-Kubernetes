2e5befd405ffc0436188878e3d3be8e54ffeffe6
Aminu <Amin4712@gmail.com>
2022-11-20 22:27:07 +0100

    optimizing ansible

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! FROM CIRCLECI THIS WORK NOW!!! Testing ansible git 003')
+    res.send('Hello World! FROM CIRCLECI THIS WORK NOW!!! Testing ansible git FINAL!!!')
 })
 
 app.listen(port, () => {
